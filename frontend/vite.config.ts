21fbd9647c5624ffdbd6fb42dc0a8a1349f8bb62
Ali <malihaider2006@gmail.com>
2025-04-14 18:24:32 +0530

    updated vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createServer } from 'http'
-import { isPortAvailable } from '../backend/src/utils/portUtils.js'
+// import { createServer } from 'http'
+// import { isPortAvailable } from '../backend/src/utils/portUtils.js'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
